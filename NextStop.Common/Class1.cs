c55b61b43824f380e21d526e0eda94f22d2b5330
Drachlowski <Andreas4231@gmx.at>
2024-11-10 22:47:43 +0100

    Added Error class for duplicate entries

--- NextStop.Common/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NextStop.Common
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ NextStop.Common/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextStop.Common.Exceptions;
+
+public class DuplicateEntityException : Exception
+{
+    public DuplicateEntityException(string entityName, object key) 
+        : base($"Entity '{entityName}' with key '{key}' already exists.") { }
+}
