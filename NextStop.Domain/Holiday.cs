980915d74fe330b62000196227257dbae7732b33
Drachlowski <Andreas4231@gmx.at>
2024-11-17 16:42:20 +0100

    Added Equals function

--- NextStop.Domain/Holiday.cs
+++ NextStop.Domain/Holiday.cs
@@ -7,4 +7,20 @@ public class Holiday(int id, string name, DateTime date, DateTime? endDate, bool
     public DateTime Date { get; set; } = date;
     public DateTime? EndDate { get; set; } = endDate;
     public bool IsSchoolHoliday { get; set; } = isSchoolHoliday;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Holiday other) return false;
+
+        return Id == other.Id &&
+               Name == other.Name &&
+               Date == other.Date &&
+               EndDate == other.EndDate &&
+               IsSchoolHoliday == other.IsSchoolHoliday;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name, Date, EndDate, IsSchoolHoliday);
+    }
 }
